f7ed9452450ba93d48ee0756583ab9031f773d50
manuel <manolo@iesvirgendelcarmen.com>
2018-12-13 10:16:31 +0100

    nuevo archivo:  src/TestTrianguloRectangulo.java 	nuevo archivo:  src/TrianguloRectangulo.java

---
+++ src/TestTrianguloRectangulo.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+
+public class TestTrianguloRectangulo {
+
+	public static void main(String[] args) {
+		//solicitamos los dos catetos con joptionpane
+		String sCateto1 = JOptionPane.showInputDialog("Introduce el valor de cateto1");
+		if ( !sCateto1.matches("[0-9]+" )) {
+			JOptionPane.showMessageDialog(null, "Valor no permitido", "ERROR",
+					JOptionPane.ERROR_MESSAGE); 
+			return;
+		}
+		String sCateto2 = JOptionPane.showInputDialog("Introduce el valor de cateto2");
+		if ( !sCateto2.matches("[0-9]+" )) {
+			JOptionPane.showMessageDialog(null, "Valor no permitido", "ERROR",
+					JOptionPane.ERROR_MESSAGE); 
+			return;
+		}
+		
+		int iCateto1 = Integer.parseInt(sCateto1);
+		int iCateto2 = Integer.parseInt(sCateto2);
+		//cosntruir un objeto de tipo TrianguloRectangulo
+		TrianguloRectangulo trianguloRectangulo = new TrianguloRectangulo(iCateto1, iCateto2);
+		
+		System.out.printf("%S, de área %.2f y de perímetro %.2f%n", trianguloRectangulo.toString(), 
+				trianguloRectangulo.calcularArea(), trianguloRectangulo.calcularPerimetro());
+		String titulo = "Información del triángulo rectángulo";
+		String mensaje = trianguloRectangulo + " " + "\nÁrea: " +
+				Math.round(	trianguloRectangulo.calcularArea() * 100) / 100.0 + "\nPerímetro: " +
+				Math.round(trianguloRectangulo.calcularPerimetro() * 100) / 100.0;
+		JOptionPane.showMessageDialog(null, mensaje, titulo, JOptionPane.PLAIN_MESSAGE); 
+	}
+
+}
---
+++ src/TrianguloRectangulo.java
@@ -0,0 +1,42 @@
+
+public class TrianguloRectangulo {
+
+	//atributos
+	private int cateto1;
+	private int cateto2;
+	//constructor
+	public TrianguloRectangulo(int cateto1, int cateto2) {
+		this.cateto1 = cateto1;
+		this.cateto2 = cateto2;
+	}
+	//getters y setters
+	public int getCateto1() {
+		return cateto1;
+	}
+	public void setCateto1(int cateto1) {
+		this.cateto1 = cateto1;
+	}
+	public int getCateto2() {
+		return cateto2;
+	}
+	public void setCateto2(int cateto2) {
+		this.cateto2 = cateto2;
+	}
+	//método que calcula la hipotenusa
+	public double calcularHipotenusa() {
+		return Math.hypot(cateto2, cateto1);
+	}
+	//método que calcula el área
+	public double calcularArea() {
+		return cateto1 * cateto2 / 2.0;
+	}
+	//método que calcula el perímetro
+	public double calcularPerimetro() {
+		return cateto1 + cateto2 + calcularHipotenusa();
+	}
+	@Override
+	public String toString() {
+		return "TrianguloRectangulo [cateto1=" + cateto1 + ", cateto2=" + cateto2 +
+				", hipotenusa=" + Math.round(calcularHipotenusa() * 100) / 100.0 + "]";
+	}
+}
